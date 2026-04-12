c94eb2b8d8dd214f89fe21e7a31f07534f2df56c
A. Snap <asnap5319@gmail.com>
2026-04-12 12:20:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8111746683-c1e57",
+  "appId": "1:1070901483498:web:47c67293702a67810cc493",
+  "apiKey": "AIzaSyAI3biHeysO2T4am-9iqDT6_k0nO-0iKL8",
+  "authDomain": "studio-8111746683-c1e57.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1070901483498"
+};
