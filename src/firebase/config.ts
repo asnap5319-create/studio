937527e284e3d3aca4405d17f0096015468d9c10
A. Snap <asnap5319@gmail.com>
2026-04-12 14:54:47 +0000

    {   "project_info": {     "project_number": "531185118307",     "project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-8111746683-c1e57",
-  "appId": "1:1070901483498:web:47c67293702a67810cc493",
-  "apiKey": "AIzaSyAI3biHeysO2T4am-9iqDT6_k0nO-0iKL8",
-  "authDomain": "studio-8111746683-c1e57.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1070901483498"
+  "projectId": "asnap-e716c",
+  "appId": "",
+  "apiKey": "AIzaSyChBzNTNFSNw0DKpTlUM0g24V5xB4Zo96c",
+  "authDomain": "asnap-e716c.firebaseapp.com",
+  "storageBucket": "asnap-e716c.firebasestorage.app",
+  "messagingSenderId": "531185118307",
+  "measurementId": ""
 };
