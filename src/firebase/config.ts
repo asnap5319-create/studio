f6d6821e300d7c24ab6caa46d1318df43193a303
A. Snap <asnap5319@gmail.com>
2026-04-21 17:23:38 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1070901483498:web:47c67293702a67810cc493",
   "apiKey": "AIzaSyAI3biHeysO2T4am-9iqDT6_k0nO-0iKL8",
   "authDomain": "studio-8111746683-c1e57.firebaseapp.com",
+  "storageBucket": "studio-8111746683-c1e57.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1070901483498"
 };
