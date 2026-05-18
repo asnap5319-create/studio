9f60b69fe46b459f1edb4b7b58d7341ac32dd366
A. Snap <asnap5319@gmail.com>
2026-05-18 16:53:13 +0000

    Jo me bolun bo hi karna us ke Aalba kuch nahi karna hai ok Please conver

---
+++ capacitor.config.ts
@@ -0,0 +1,18 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.asnap.app',
+  appName: 'A.snap',
+  webDir: 'out',
+  server: {
+    url: 'https://studio-xi-henna-41.vercel.app',
+    cleartext: true,
+    allowNavigation: ['studio-xi-henna-41.vercel.app']
+  },
+  android: {
+    allowMixedContent: true
+  }
+};
+
+export default config;
